5d3ff3e17ae1c7d0bae252eb20e42815e493ab06
jeisux92 <jei.sum41@gmail.com>
2019-09-05 17:26:54 -0500

    exceptions

---
+++ result/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io;
+use std::io::ErrorKind;
+use std::io::Read;
+
+fn main() {
+    let f = File::open("hello.txt").unwrap();
+    println!("{:#?}", f);
+}
+
+fn match_file() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+    println!("{:#?}", f);
+}
+
+fn without_match_file() {
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+    println!("{:#?}", f);
+}
